05b0dc83e7896d0c2d6c82fa747533c10cc95951
vlasov <stea1th@mail.ru>
2020-02-06 10:04:43 +0100

    Projektdateien hinzufügen.

---
+++ ChessBoard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestChess
+{
+    class ChessBoard
+    {
+
+        private readonly int _boardSize = 8;
+        private readonly string _threeSpaces = "   ";
+        private readonly string _twoSpaces = "  ";
+        private readonly string _oneSpace = " ";
+
+        public void printForWhiteNumbers()
+        {
+
+        } 
+
+    }
+}
